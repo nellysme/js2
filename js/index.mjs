5c0525f18d19b14264ccdfed6843ae4647aa7373
nellysme <nelly1@live.no>
2023-10-18 16:53:49 +0200

    added mjs

--- js/index.mjs
+++ js/index.mjs
@@ -1,10 +1,26 @@
-import { setRegisterFormListner } from "./handlers/register.mjs";
-import { setLoginFormListner } from "./handlers/login.mjs";
+import * as listeners from "./handlers/index.mjs";
+import * as templates from "./templates/index.mjs";
+import * as postMethods from "./api/posts/index.mjs";
 
 const path = location.pathname;
 
+console.log(path)
+
 if (path === "/index.html") {
-  setLoginFormListner();
-} else if (path === "/register/index.html") {
-  setRegisterFormListner();
+  listeners.setLoginFormListner();
+} else if (path === "/register/") {
+  listeners.setRegisterFormListner();
+} else if (path === '/post/create/'){
+  listeners.setCreatePostFormListener();
+} else if (path === '/post/edit/') {
+
+  listeners.setUpdatePostListener();
 }
+
+//async function testTemplate() {
+//  const posts = await postMethods.getPosts();
+//  const container = document.querySelector("#post");
+//  templates.renderPostTemplates(posts, container);
+//}
+//
+//testTemplate();
